2cb59921826ad58b1e9481c86b7ad498d1449396
Fiorella RC <fiorellarodriguez05.fr@gmail.com>
2021-04-26 23:30:09 +0000

    creacion tablas

---
+++ tabla_carga.sql
@@ -0,0 +1,13 @@
+CREATE TABLE carga (
+	ProductID INT ,
+	ProductName varchar(50),
+	CompanyName varchar(50),
+	City  varchar(50),
+	Region varchar(50),
+	Country varchar(50),
+	CategoryName varchar(50),
+	QuantityPerUnit INT,
+	UnitPrice numeric(10,2),
+	UnitsOnOrder INT,
+	Dicontinued bit
+)
---
+++ tabla_registro.sql
@@ -0,0 +1,6 @@
+CREATE TABLE registro (
+	cargaArchivo datetime,
+	cantLineas INT,
+	cargaExistosa bit,
+	mensajeError varchar(100)
+)
